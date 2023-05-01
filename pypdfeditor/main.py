4dc3676b31c1dd3bde7e219b53782d8b01ae32f2
Alexander Alarcon <walarcon14@gmail.com>
2023-05-01 16:03:45 -0500

    feat: read args for split command

--- pypdfeditor/main.py
+++ pypdfeditor/main.py
@@ -1,8 +1,66 @@
 #!/usr/bin/env python3
 
+import argparse
+
+from typing import NamedTuple
+from enum import StrEnum
+
+
+class Command(StrEnum):
+    SPLIT = "split"
+
+
+class SplitArgs(NamedTuple):
+    source_file: str
+    page_range: str
+
+
+class Args(NamedTuple):
+    command: Command
+    options: SplitArgs
+
+
+def read_args() -> Args:
+    parser = argparse.ArgumentParser(description="Python PDF editor")
+    subparser: argparse._SubParsersAction = parser.add_subparsers(
+        dest="command",
+        required=True,
+    )
+
+    split_parser: argparse.ArgumentParser = subparser.add_parser(
+        Command.SPLIT, help="Split a PDF file into multiple files based on page ranges."
+    )
+    split_parser.add_argument("source_file", help="Path to the source PDF file")
+    split_parser.add_argument(
+        "-r",
+        "--range",
+        help="Specify page ranges to split on (e.g. '1-5,8-10')",
+        required=True,
+    )
+
+    args: argparse.Namespace = parser.parse_args()
+
+    if args.command == Command.SPLIT:
+        return Args(
+            command=args.command,
+            options=SplitArgs(
+                source_file=args.source_file,
+                page_range=args.range,
+            ),
+        )
+    else:
+        raise argparse.ArgumentError(None, "Error: Invalid command")
+
 
 def main() -> None:
-    print("Hello world")
+    try:
+        args: Args = read_args()
+        match args.command:
+            case Command.SPLIT:
+                print("Split command was selected")
+
+    except argparse.ArgumentError as e:
+        print(str(e))
 
 
 if __name__ == "__main__":
