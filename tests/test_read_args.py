676df6025b519a53cbb217f1e142687beea060c0
Alexander Alarcon <walarcon14@gmail.com>
2023-05-07 08:47:22 -0500

    test: Add tests for merge command in read_args.py

--- tests/test_read_args.py
+++ tests/test_read_args.py
@@ -1,9 +1,8 @@
 import pytest
-
 from pytest import MonkeyPatch
 
-from pypdfeditor.type_definitions import Args, Command, SplitArgs
 from pypdfeditor.read_args import read_args
+from pypdfeditor.type_definitions import Args, Command, MergeArgs, SplitArgs
 
 
 class TestReadArgs:
@@ -54,5 +53,45 @@ class TestReadArgs:
         expected_args: Args = Args(
             command=Command.SPLIT,
             options=SplitArgs(source_file="test.pdf", pages="1-3"),
+        )
+        assert args == expected_args
+
+    def test_read_args_merge_without_arguments(self, monkeypatch: MonkeyPatch) -> None:
+        """
+        Test case: should raise exception when merge command is provided without required arguments
+        """
+        monkeypatch.setattr(
+            "sys.argv",
+            ["pypdfeditor", "merge"],
+        )
+        with pytest.raises(SystemExit):
+            read_args()
+
+    def test_read_args_merge_with_arguments(self, monkeypatch: MonkeyPatch) -> None:
+        """
+        Test case: should return Args object when split command is provided with required arguments
+        """
+        monkeypatch.setattr(
+            "sys.argv",
+            [
+                "pypdfeditor",
+                "merge",
+                "-o",
+                "merged.pdf",
+                "-i",
+                "file1.pdf",
+                "-i",
+                "file2.pdf",
+                "-i",
+                "file3.pdf",
+            ],
+        )
+        args: Args = read_args()
+        expected_args: Args = Args(
+            command=Command.MERGE,
+            options=MergeArgs(
+                output_file="merged.pdf",
+                input_files=["file1.pdf", "file2.pdf", "file3.pdf"],
+            ),
         )
         assert args == expected_args
